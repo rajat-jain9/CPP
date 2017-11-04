8bc17c70140e82bccc99ea5419b41148ae28621d
rajat-jain9 <jainrajatcse@gmail.com>
2017-11-04 11:48:33 +0530

    added cpp test program

---
+++ CPP1/Hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a = 5;
+	cout << "Entered value is = " << a << endl;
+	return 0;
+}
+
+/*Ctrl+shift+b  >> C++ (To compile)
+Ctrl+shift+b >> C++ run (To Run)
+			OR
+tools>>build with>>enter>>option will come*/
